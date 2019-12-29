f878745b7c81964111fe2ae38cb05db0b39be888
yueshen1269 <1152555377@qq.com>
2019-12-29 17:19:50 +0800

    引入prettier，增加默认样式，全局样式，编写头部和tab

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import { GlobalStyle } from  './style';
-import { IconStyle } from "./assets/iconfont/iconfont"
+import React from "react";
+import { HashRouter } from "react-router-dom";
+import { renderRoutes } from "react-router-config";
+
+import { GlobalStyle } from "./style";
+import { IconStyle } from "./assets/iconfont/iconfont";
+import routes from "./routes/index";
+
 function App() {
   return (
-    <div className="App">
+    <HashRouter>
       <GlobalStyle></GlobalStyle>
       <IconStyle></IconStyle>
-      <i className="iconfont">&#xe62b;</i>
-      <i className="iconfont icon-jiantou"></i>
-    </div>
+      {renderRoutes(routes)}
+    </HashRouter>
   );
 }
 
