40487b45c36580e65a9472aa31629ceaf3990690
shenyue <1152555377@qq.com>
2020-01-01 22:06:40 +0800

    <feat> 新增歌手横滚动条

--- src/application/Singers/index.js
+++ src/application/Singers/index.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
+import Horizen from '../../baseUI/horizen-item';
+import { categoryTypes, alphaTypes } from '../../api/config';
+import { NavContainer } from "./style";
 
 function Singers(props) {
-  return <div>Singers</div>;
+  let [category, setCategory] = useState ('');
+  let [alpha, setAlpha] = useState ('');
+
+  let handleUpdateAlpha = (val) => {
+    setAlpha (val);
+  }
+
+  let handleUpdateCatetory = (val) => {
+    setCategory (val);
+  }
+
+  return (
+    <NavContainer>
+      <Horizen
+        list={categoryTypes}
+        title={"分类 (默认热门):"}
+        handleClick={(val) => handleUpdateCatetory (val)}
+        oldVal={category}></Horizen>
+      <Horizen
+        list={alphaTypes}
+        title={"首字母:"}
+        handleClick={val => handleUpdateAlpha (val)}
+        oldVal={alpha}></Horizen>
+    </NavContainer>
+  )
 }
 
 export default React.memo(Singers);
